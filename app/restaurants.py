6cf6ee764eb8d0a2ab141b6a8a5b2ff6aeb53f6e
Hproud <havin.becca.93@gmail.com>
2024-03-06 23:47:36 -0600

    added restaurants to seeder

---
+++ app/restaurants.py
@@ -0,0 +1,84 @@
+{
+"id": 1,
+"name": "The Loop Brewing Co",
+"logo": "https://loopbrewingcompany.com/wp-content/uploads/2023/11/LoopBrewingCompany.png",
+"address": "404 W A st",
+"city": "McCook",
+"state": "Nebraska",
+"zip_code": 69001,
+"owner_id": 1
+
+},
+{
+"id": 2,
+"name": "Applebee`s Bar and Grill",
+"logo": "https://assets.restaurants.applebees.com/images/logo__full-logo__color.svg?iar=0&hash=4FE124FB9586F5159DF7A6635E130997",
+"address": "5605 2nd Ave",
+"city": "Kearney",
+"state": "Nebraska",
+"zip_code":68847,
+"owner_id": 3
+
+},
+{
+"id": 3,
+"name": "El Puerto",
+"logo": "https://scontent-den2-1.xx.fbcdn.net/v/t39.30808-1/298766776_581075440381423_8748120028866658599_n.jpg?stp=cp0_dst-jpg_e15_p120x120_q65&_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=W4kP-jvfyLMAX8Yrv2g&_nc_ht=scontent-den2-1.xx&oh=00_AfCjLnMfiHIXNQLRs3Dl3XuMcbNfwezcDVz8Hopem9imVQ&oe=65EEF5BF",
+"address": "311 Norris Ave",
+"city": "McCook",
+"state": "Nebraska",
+"zip_code": 69001,
+"owner_id": 2
+},
+{
+"id": 4,
+"name": "Fresh Express",
+"logo": "https://lh3.googleusercontent.com/p/AF1QipNNGGlg7e4nRJPNWsXizz__q4lL094YgE_imKrQ=s680-w680-h510",
+"address": "3719 2nd Ave",
+"city": "Kearney",
+"state": "Nebraska",
+"zip_code": 68847,
+"owner_id": 1
+},
+{
+"id": 5,
+"name": "Pham`s Coffee & Boba",
+"logo": "https://lh3.googleusercontent.com/p/AF1QipNhWOI3gJo_qbFJ6gyhfoYhtPBE4u2FLfxC5ylC=s680-w680-h510",
+"address": "614 N Eddy st",
+"city": "Grand Island",
+"state": "Nebraska",
+"zip_code": 68801,
+"owner_id": 3
+},
+{
+"id": 6,
+"name": "Citta Deli",
+"logo": "https://lh3.googleusercontent.com/a-/ALV-UjV_HjsRI27xzFBdicKe7VLnl8n_1gOVsmWiyNdpZVvDfA=w60-h60-p-rp-mo-br100",
+"address": "110 W 1st st",
+"city": "McCook",
+"state": "Nebraska",
+"zip_code": 69001,
+"owner_id": 2
+},
+{
+"id": 7,
+"name": "Pizza Hut",
+"logo": "https://www.pizzahut.com/assets/w/images/Logo_PH_Retro@2x.png",
+"address": "811 US-83",
+"city": "McCook",
+"state": "Nebraska",
+"zip_code": 69001,
+"owner_id": 2
+
+
+},
+{
+"id": 8,
+"name": "Joes Crab Shack",
+"logo": "https://images.getbento.com/accounts/edf5b1bb2b8913719a3af84304d86c2e/media/images/89035joes-crab-shack-logo.png",
+"address": "5802 W Loop South #289",
+"city": "Lubbock",
+"state": "Texas",
+"zip_code": 79424,
+"owner_id": 3
+}
