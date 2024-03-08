8697156ff26d1c13ba3857d8b3498ef717bb1aed
root <root@Vincent-GalaxyBook2Pro>
2024-03-07 19:58:11 -0800

    review model 1st draft

---
+++ app/models/review.py
@@ -0,0 +1,13 @@
+from .db import db, environment, SCHEMA
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+
+    user = db.relationship('User', back_populates='reviews')
