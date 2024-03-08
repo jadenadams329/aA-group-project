6f4e9b29632ae3f08e42c484c347ac2bef23c2d7
Hproud <havin.becca.93@gmail.com>
2024-03-07 22:32:52 -0600

    finished models for carts

---
+++ app/models/cart.py
@@ -0,0 +1,18 @@
+from .db import db,SCHEMA,environment
+from sqlalchemy.schema import Column,ForeignKey
+from sqlalchemy.types import Integer,Boolean,Date
+from sqlalchemy.orm import relationship
+
+class Cart(db.Model):
+    __tablename__ = 'carts'
+
+    id=Column(Integer,primary_key=True)
+    user_id=Column(Integer,ForeignKey=("user_id"),nullable=False)
+    restaurant_id=Column(Integer,ForeignKey=("restaurant_id"),nullable=True)
+    purchased=Column(Boolean,default=False)
+    created_at=Column(Date)
+    last_updated_at=Column(Date)
+
+    cartitems = relationship("CartItem", back_populates="cart")
+    user=relationship("User", back_populates="carts")
+    restaurant= relationship("Restaurant", back_populates="carts")
---
+++ app/models/cart_items.py
@@ -0,0 +1,20 @@
+from .db import db,SCHEMA,environment
+from sqlalchemy.schema import Column,ForeignKey
+from sqlalchemy.types import Integer
+from sqlalchemy.orm import relationship
+
+
+class CartItem(db.Model):
+    __tablename__='cartitems'
+
+    if environment == "production":
+     __table_args__ = {'schema': SCHEMA}
+
+
+    id=Column(Integer,primary_key=True)
+    cart_id=Column(Integer,ForeignKey("cart_id"),nullable=False)
+    menu_item_id=Column(Integer,ForeignKey("menuItems_id"),nullable=False)
+    quantity=Column(Integer,default=1)
+
+    cart = relationship("Cart",back_populates="cartitems")
+    menu_item = relationship("MenuItem", back_populates="cart_item")
