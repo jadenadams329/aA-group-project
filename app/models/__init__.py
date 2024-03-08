ad63d0929dc3ce35ddfc8ef21bf80d2710f2b9bb
root <root@Vincent-GalaxyBook2Pro>
2024-03-07 21:43:45 -0800

    seeders for restaurant and reviews

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,5 @@
 from .db import db
 from .user import User
+from .restaurant import Restaurant
+from .review import Review
 from .db import environment, SCHEMA
--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .reviews import seed_reviews, undo_reviews
+from .restaurants import seed_restaurants, undo_restaurants
 
 from app.models.db import db, environment, SCHEMA
 
@@ -17,8 +19,11 @@ def seed():
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
+        undo_restaurants()
+        undo_reviews()
     seed_users()
-    
+    seed_restaurants()
+    seed_reviews()
     # Add other seed functions here
 
 
@@ -26,4 +31,6 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_restaurants()
+    undo_reviews()
     # Add other undo functions here
