f6455c233dad661fd7f5de97683ba47a59a2d40f
root <root@Vincent-GalaxyBook2Pro>
2024-03-07 20:46:52 -0800

    fixed columns

--- app/models/review.py
+++ app/models/review.py
@@ -9,5 +9,7 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+    stars = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String, nullable=False)
 
     user = db.relationship('User', back_populates='reviews')
