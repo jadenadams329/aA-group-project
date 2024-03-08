95dcc9b8494a531271e8204e91327fe7e7c9a709
root <root@Vincent-GalaxyBook2Pro>
2024-03-07 19:43:05 -0800

    restaurant model added

---
+++ app/models/restaurant.py
@@ -0,0 +1,18 @@
+from .db import db, SCHEMA, environment
+
+class Restaurants(db.Model):
+    __tablename__ = 'restaurants'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    logo = db.Column(db.String)
+    address = db.Column(db.String, nullable=False)
+    city = db.Column(db.String, nullable=False)
+    state = db.Column(db.String, nullable=False)
+    zip_code = db.Column(db.Integer, nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    
+    user = db.relationship('User', back_populates='restaurants')
