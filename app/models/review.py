634c474c46e76ae11abac5ad14b1337f8ae2b22c
Hproud <havin.becca.93@gmail.com>
2024-03-09 14:54:34 -0600

    fixed associations

--- app/models/review.py
+++ app/models/review.py
@@ -7,9 +7,10 @@ class Review(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id",ondelete='CASCADE'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id",ondelete='CASCADE'))
     stars = db.Column(db.Integer, nullable=False)
     review = db.Column(db.String, nullable=False)
 
     user = db.relationship('User', back_populates='reviews')
+    restaurant = db.relationship('Restaurant', back_populates='reviews')
