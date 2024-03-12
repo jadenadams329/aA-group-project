54a98ff2cd6a2c1978be9970aab9b283bab514fe
Jaden <jadenadams329@gmail.com>
2024-03-11 19:33:42 -0700

    some styling done but nowhere near complete

--- react-vite/src/components/LoginFormPage/LoginFormPage.jsx
+++ react-vite/src/components/LoginFormPage/LoginFormPage.jsx
@@ -5,62 +5,56 @@ import { Navigate, useNavigate } from "react-router-dom";
 import "./LoginForm.css";
 
 function LoginFormPage() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const sessionUser = useSelector((state) => state.session.user);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState({});
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const sessionUser = useSelector((state) => state.session.user);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [errors, setErrors] = useState({});
 
-  if (sessionUser) return <Navigate to="/" replace={true} />;
+	if (sessionUser) return <Navigate to='/' replace={true} />;
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    const serverResponse = await dispatch(
-      thunkLogin({
-        email,
-        password,
-      })
-    );
+		const serverResponse = await dispatch(
+			thunkLogin({
+				email,
+				password,
+			})
+		);
 
-    if (serverResponse) {
-      setErrors(serverResponse);
-    } else {
-      navigate("/");
-    }
-  };
+		if (serverResponse) {
+			setErrors(serverResponse);
+		} else {
+			navigate("/");
+		}
+	};
 
-  return (
-    <>
-      <h1>Log In</h1>
-      {errors.length > 0 &&
-        errors.map((message) => <p key={message}>{message}</p>)}
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.password && <p>{errors.password}</p>}
-        <button type="submit">Log In</button>
-      </form>
-    </>
-  );
+	return (
+		<>
+			<div className="loginFormContainer">
+				<div>
+					<h1 style={{ float: 'left', fontWeight: '500'}}>Yum </h1>
+					<h1 style={{ float: 'right', marginLeft: '5px'}}>Cart</h1>
+				</div>
+				{errors.length > 0 && errors.map((message) => <p key={message}>{message}</p>)}
+				<form onSubmit={handleSubmit}>
+					<label>
+						Email
+						<input type='text' value={email} onChange={(e) => setEmail(e.target.value)} required />
+					</label>
+					{errors.email && <p>{errors.email}</p>}
+					<label>
+						Password
+						<input type='password' value={password} onChange={(e) => setPassword(e.target.value)} required />
+					</label>
+					{errors.password && <p>{errors.password}</p>}
+					<button type='submit'>Log In</button>
+				</form>
+			</div>
+		</>
+	);
 }
 
 export default LoginFormPage;
