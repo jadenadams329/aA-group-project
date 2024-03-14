2dd4158c9d385ecb9b4a49ca0fbe7089b7df1937
Hproud <havin.becca.93@gmail.com>
2024-03-13 22:11:46 -0500

    working on cart routes

--- app/api/cart_routes.py
+++ app/api/cart_routes.py
@@ -3,17 +3,21 @@ from app.models.Cart import Cart
 from app.models.db import db
 from app.models.Cart_Items import CartItem
 from flask.json import jsonify
+from app.models.menu_item import MenuItem
 
 
 cart_route = Blueprint('cart_route',__name__)
 
-@cart_route.route('/')
+@cart_route.route('/items')
 def fullCart():
     allthem=CartItem.query.all()
     theFullList = []
     for item in allthem:
         theguy = item.to_dict()
+        menu_items = int(theguy["menu_item_id"])
+        menu_item = MenuItem.query.filter_by(menu_item_id: menu_items)
         theFullList.append(theguy)
-        print(theguy,"this is the item")
+        print(int(theguy["menu_item_id"]),"this is the item")
+        print(menu_items)
 
     return theFullList
