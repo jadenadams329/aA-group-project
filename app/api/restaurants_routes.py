d817666a8ec0c201db3b446caba1dced67018a50
Jaden <jadenadams329@gmail.com>
2024-03-14 19:19:15 -0700

    restaurant by id backend done, also added file for frontend

--- app/api/restaurants_routes.py
+++ app/api/restaurants_routes.py
@@ -2,12 +2,28 @@ from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import Restaurant
 
-restaurant_routes = Blueprint('restaurants', __name__)
+restaurant_routes = Blueprint("restaurants", __name__)
 
-@restaurant_routes.route('')
+
+@restaurant_routes.route("")
 def get_restaurants():
     try:
         restaurants = Restaurant.query.all()
-        return {'restaurants': [restaurant.to_dict() for restaurant in restaurants]}
+        return {
+            "restaurants": [restaurant.to_dict() for restaurant in restaurants]
+        }, 200
+    except Exception as err:
+        return jsonify(error=str(err)), 500
+
+
+@restaurant_routes.route("/<int:id>", methods=["GET"])
+def get_restaurant_by_id(id):
+    try:
+        restaurant = Restaurant.query.get(id)
+        print(id)
+        if not restaurant:
+            return jsonify(error="Restaurant not found"), 404
+
+        return restaurant.to_dict(), 200
     except Exception as err:
         return jsonify(error=str(err)), 500
--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -3,6 +3,7 @@ import LoginFormPage from '../components/LoginFormPage';
 import SignupFormPage from '../components/SignupFormPage';
 import RestaurantsPage from '../components/RestaurantsPage/RestaurantsPage';
 import Layout from './Layout';
+import RestaurantDetailsPage from '../components/RestaurantDetailsPage/RestaurantDetailsPage';
 
 export const router = createBrowserRouter([
   {
@@ -23,6 +24,10 @@ export const router = createBrowserRouter([
       {
         path: "restaurants",
         element: <RestaurantsPage />
+      },
+      {
+        path: "restaurants/:id",
+        element: <RestaurantDetailsPage/>
       }
     ],
   },
