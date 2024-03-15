a2b3fb2cd36126e1e0484fe617b1394315404ba8
Jaden <jadenadams329@gmail.com>
2024-03-14 20:23:04 -0700

    added restaurant state for restaurant details page

--- react-vite/src/components/RestaurantDetailsPage/RestaurantDetailsPage.jsx
+++ react-vite/src/components/RestaurantDetailsPage/RestaurantDetailsPage.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import {  useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getRestaurant } from '../../redux/restaurants';
 
 function RestaurantDetailsPage() {
+  const dispatch = useDispatch();
+	const { id } = useParams();
+
+  const restaurant = useSelector((state) => state.restaurants.data[id])
+  console.log(restaurant)
+
+  useEffect(() => {
+    dispatch(getRestaurant(id))
+  }, [dispatch, id])
+
   return (
     <div>RestaurantDetailsPage</div>
   )
